310129afbf7225a4976623b0ae6c28344ba78382
Luciana Caruso <55926005+Lucha2040@users.noreply.github.com>
2020-01-17 21:02:08 +0100

    Javascript 3 Homework Week 2

---
+++ javascript/javascript3/week3/main.js
@@ -0,0 +1,31 @@
+
+const redBox = document.querySelector('ul.marks li:nth-child(1)');
+const blueBox = document.querySelector('ul.marks li:nth-child(2)');
+const greenBox = document.querySelector('ul.marks li:nth-child(3)');
+
+
+translateAllAtOnce()
+function translateAllAtOnce () { 
+  Promise.all[moveElement(redBox, {x: 20, y: 300}), moveElement(blueBox, {x: 400, y: 300}), moveElement(greenBox, {x: 400, y: 20})]
+}
+
+// translateOneByOne()
+function translateOneByOne () {
+   new Promise((resolve) => {
+      resolve(moveElement(redBox, {x: 20, y:300}))
+}
+    ).then(()=>  {
+     return new Promise((resolve) => {
+       resolve(moveElement(blueBox, {x: 400, y:300}))
+      }
+      )  
+  }
+  ).then(()=>  {
+   return  new Promise((resolve) => {
+      resolve(moveElement(greenBox, {x: 400, y:20}))
+     }
+     )  
+ })
+}
+
+
