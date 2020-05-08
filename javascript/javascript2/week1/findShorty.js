d0667a75679f1b9c143835fc5bd8ec1e48d79594
Luciana Caruso <lucianac2005@gmail.com>
2020-05-08 23:55:59 +0200

    Implemented feedback. Used map and filter instead (28:36)

--- javascript/javascript2/week1/findShorty.js
+++ javascript/javascript2/week1/findShorty.js
@@ -10,22 +10,27 @@ const danishWords = [
     "øl",
     "egentlig",
   ];
-  const wordsBySize = [];
   
-  function getShorty(a) {
-  //First we do an organized array with the length of the words  
-    for (i = 0; i < a.length; i++) {
-      let wordSize = a[i].length;
-      wordsBySize.push(wordSize);
-      wordsBySize.sort(function(a, b){return a - b}); //this part organizes min to max.
-      }
-  //Now we search for the first word of our organized array that matches our shorty word
-    for (i = 0; i < a.length; i++) {
-      if (wordsBySize[0] === a[i].length) {
-          return a[i]
-      }
-    }
-  }
+  const spanishWords = [
+    "hola",
+    "Argentina",
+    "café",
+    "amor",
+    "gato",
+    "perro",
+    "y",
+    "empanadas",
+    "asado",
+    "cerveza",
+  ];
   
-  console.log(getShorty(danishWords)) // Øl length = 2 
+
+function getShorty(arr) {  
+  const getLength = arr.map((el) => {return el.length})
+  const getShortest = Math.min.apply(null, getLength)
+  const shorty = arr.filter((el) => {return el.length === getShortest})
+  return shorty
+}
   
+ getShorty(danishWords) 
+ getShorty(spanishWords)
