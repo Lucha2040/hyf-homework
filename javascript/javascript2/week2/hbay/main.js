b8d3d40ba29624a430dda9e20188ee91742abc29
Luciana Caruso <lucianac2005@gmail.com>
2020-05-12 01:11:35 +0200

    Added HYF Bay exercise

---
+++ javascript/javascript2/week2/hbay/main.js
@@ -0,0 +1,45 @@
+console.log("Script loaded");
+
+function renderProducts(arr) {
+  const mainUl = document.querySelector("section.products ul");
+  arr.forEach((product) => {
+    const productsLi = document.createElement("li");
+    const productsUl = document.createElement("ul");
+
+    const nameLi = document.createElement("li");
+    nameLi.innerHTML = product.name;
+    productsUl.appendChild(nameLi);
+
+    const priceLi = document.createElement("li");
+    priceLi.innerHTML = product.price;
+    productsUl.appendChild(priceLi);
+
+    const ratingLi = document.createElement("li");
+    ratingLi.innerHTML = product.rating;
+    productsUl.appendChild(ratingLi);
+
+    const shippingLi = document.createElement("li");
+    const shippingUl = document.createElement("ul");
+    product.shipsTo.forEach((country) => {
+      const countryLi = document.createElement("li");
+      countryLi.innerHTML = country;
+      shippingUl.appendChild(countryLi);
+      shippingLi.appendChild(shippingUl);
+    });
+    productsUl.appendChild(shippingLi);
+    productsLi.appendChild(productsUl);
+    mainUl.appendChild(productsLi);
+  });
+}
+
+let availableProducts = getAvailableProducts();
+
+const input = document.getElementById("searchInput");
+
+input.addEventListener("input", () => {
+  let searchedProduct = input.value.toLowerCase();
+  let searchingArr = availableProducts.filter((product) =>
+    product.name.toLowerCase().includes(searchedProduct)
+  );
+  renderProducts(searchingArr);
+});
