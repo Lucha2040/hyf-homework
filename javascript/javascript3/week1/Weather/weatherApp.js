6dce094b6ca2ec1f2b321d8ba895c8d947a01f7d
Luciana Caruso <lucianac2005@gmail.com>
2020-05-21 17:39:13 +0200

    Implemented feedback adding and converting units

--- javascript/javascript3/week1/Weather/weatherApp.js
+++ javascript/javascript3/week1/Weather/weatherApp.js
@@ -8,22 +8,25 @@ searchBtn.addEventListener("click", () => {
 
 function fetchData(city) {
   return fetch(
-    `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${keyApi}`
+    `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${keyApi}&units=metric`
   )
     .then((response) => {
       return response.json();
     })
     .then((data) => {
+      console.log(data)
       const logIcon = document.getElementById("icon");
       const logoCode = data.weather[0].icon;
       const icon = `http://openweathermap.org/img/wn/${logoCode}@2x.png`;
+      const sunrise = new Date (data.sys.sunrise*1000)
+      const sunset = new Date(data.sys.sunset*1000)
       const infoToHTML = `<p>City: ${data.name}</p> 
         <p>Overview: ${data.weather[0].main}</p> 
-        <p>Temperature: ${data.main.temp}</p> 
-        <p>Wind speed: ${data.wind.speed}</p> 
+        <p>Temperature: ${data.main.temp}° C</p> 
+        <p>Wind speed: ${data.wind.speed} km/h</p> 
         <p>Cloudiness: ${data.clouds.all}%</p> 
-        <p>Sunrise: ${data.sys.sunrise}</p> 
-        <p>Sunset: ${data.sys.sunset}</p>`;
+        <p>Sunrise: ${sunrise}</p> 
+        <p>Sunset: ${sunset}</p>`;
       const render = document.getElementById("root");
       render.innerHTML = infoToHTML;
       logIcon.setAttribute("src", icon);
